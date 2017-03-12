796e315c94438d76dfe790e1ccdc1db93fbb7060
MarcinKruszynski <martin.kruszynski@gmail.com>
2017-03-13 00:19:55 +0100

    HoloVision 4 - gesture (feedback cursor, air tap)

---
+++ HoloVision4/Assets/Scripts/CubeCommands.cs
@@ -0,0 +1,16 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+
+public class CubeCommands : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputEventData eventData)
+    {
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+}
+
