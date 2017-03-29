443d401f372055aab1946208fbd52baae74a2001
MarcinKruszynski <martin.kruszynski@gmail.com>
2017-03-29 22:46:34 +0200

    HoloVision8 - color room

---
+++ HoloVision8/Assets/Scripts/ItemKeywords.cs
@@ -0,0 +1,28 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(Renderer))]
+public class ItemKeywords : MonoBehaviour, ISpeechHandler
+{
+    public void OnShoot()
+    {
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechKeywordRecognizedEventData eventData)
+    {
+        switch (eventData.RecognizedText.ToLower())
+        {
+            case "shoot down":
+                OnShoot();
+                break;
+        }
+    }
+}
